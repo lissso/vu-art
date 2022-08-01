918a3695f3cb27bb9cda0a31cdcfb71cd4480b50
lissso <66426656+lissso@users.noreply.github.com>
2022-08-01 19:38:34 +0200

    emailjs

--- client/src/Components/form.js
+++ client/src/Components/form.js
@@ -11,6 +11,11 @@ export default function ContactForm() {
     const [status, setStatus] = useState("Submit");
     const [artists, setArtists] = useState();
 
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+    const [fromEmail, setFromEmail] = useState("");
+    const [emailArtist, setEmailArtist] = useState("");
+
     useEffect(() => {
         getArtists().then((x) => {
             console.log("getArtists", x);
@@ -21,22 +26,26 @@ export default function ContactForm() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setStatus("Sending...");
-        const { name, email, message } = e.target.elements;
-        let details = {
-            name: name.value,
-            email: email.value,
-            message: message.value,
-        };
-        let response = await fetch("http://localhost:5000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(details),
-        });
+        console.log("sending", name, message, fromEmail, emailArtist);
+        let response = await fetch(
+            "https://api.emailjs.com/api/v1.0/email/send",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    service_id: "service_cihft5a",
+                    user_id: "1wqjw3-9d15VoaV-5",
+                    template_id: "template_c3o3uup",
+                    template_params: {
+                        from_name: name,
+                        message: message,
+                        reply_to: fromEmail,
+                        email_artist: emailArtist,
+                    },
+                }),
+            }
+        );
         setStatus("Submit");
-        let result = await response.json();
-        alert(result.status);
     };
 
     return (
@@ -52,32 +61,51 @@ export default function ContactForm() {
                     <form onSubmit={handleSubmit}>
                         <div>
                             <label htmlFor="name">Name:</label>
-                            <input type="text" id="name" required />
+                            <input
+                                type="text"
+                                id="name"
+                                required
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" required />
+                            <input
+                                type="email"
+                                id="email"
+                                required
+                                value={fromEmail}
+                                onChange={(e) => setFromEmail(e.target.value)}
+                            />
                         </div>
                         <div>
                             <label htmlFor="message">Message:</label>
-                            <textarea id="message" required />
+                            <textarea
+                                id="message"
+                                required
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                            />
                         </div>
-                        <div>
+                        <div
+                            onChange={(e) => {
+                                setEmailArtist(e.target.value);
+                            }}
+                        >
                             {artists &&
                                 artists.map((x, i) => (
                                     <>
                                         <input
                                             className="checkbox"
                                             key={i}
-                                            type="checkbox"
-                                            id="artist"
-                                            name={x.name}
+                                            type="radio"
+                                            name={"artist"}
                                             value={x.email}
                                         />
                                         <label key={"label-" + i}>
-                                            {" "}
                                             {x.name}
-                                        </label>{" "}
+                                        </label>
                                     </>
                                 ))}
                             <br />
